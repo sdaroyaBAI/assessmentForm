539680327c5a09685959c31e86eac3d9db83ca13
Carl John Rodolfo <crodolfo@blastasia.com>
2016-03-30 10:09:00 +0800

    added files

--- CloudPhoenix/app.js
+++ CloudPhoenix/app.js
@@ -3,6 +3,26 @@ angular.module('CloudPhoenix', ['ui.bootstrap','ui.utils','ui.router','ngAnimate
 angular.module('CloudPhoenix').config(function($stateProvider, $urlRouterProvider) {
 
     /* Add New States Above */
+    
+    $stateProvider.state('company', {
+        url: '/company',
+        templateUrl: 'partial/company/company.html',
+        controller: 'CompanyCtrl',
+        controllerAs: 'ctrl'
+    });
+    $stateProvider.state('server', {
+        url: '/server',
+        templateUrl: 'partial/server/server.html',
+        controller: 'ServerCtrl',
+        controllerAs: 'ctrl'
+    });
+    $stateProvider.state('survey', {
+        url: '/survey',
+        templateUrl: 'partial/survey/survey.html',
+        controller: 'SurveyCtrl',
+        controllerAs: 'ctrl'
+    });
+    
     $urlRouterProvider.otherwise('/home');
 
 });
