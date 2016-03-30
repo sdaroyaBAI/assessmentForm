42966f7c13163de32c8509589d108559e80ad099
tantarantantan <jbcarino@gmail.com>
2016-03-30 14:19:32 +0800

    api cpntrollers

---
+++ src/CloudPhoenix/CloudPhoenix.UI/Api/ServerController.cs
@@ -0,0 +1,28 @@
+﻿using CloudPhoenix.Infra.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CloudPhoenix.UI.Api
+{
+    public class ServerController : ApiController
+    {
+        private ICompanyRepository _comRepo;
+        public ServerController(ICompanyRepository comRepo)
+        {
+            _comRepo = comRepo;
+        }
+        public IHttpActionResult Get(int id)
+        {
+            var uspParam = new Dictionary<string, object>();
+
+            uspParam.Add("@companyId", id);
+            var results = _comRepo.GetEntity("usp_company_server_list", uspParam);
+
+            return Ok(new { servers = results });
+        }
+    }
+}
---
+++ src/CloudPhoenix/CloudPhoenix.UI/Api/SurveyController.cs
@@ -0,0 +1,29 @@
+﻿using CloudPhoenix.Infra.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CloudPhoenix.UI.Api
+{
+    public class SurveyController : ApiController
+    {
+        private ICompanyRepository _comRepo;
+        public SurveyController(ICompanyRepository comRepo)
+        {
+            _comRepo = comRepo;
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var uspParam = new Dictionary<string, object>();
+
+            uspParam.Add("@companyId", id);
+            var results = _comRepo.GetEntity("usp_company_survey_get", uspParam);
+
+            return Ok(new { servers = results });
+        }
+    }
+}
