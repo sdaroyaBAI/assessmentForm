622c8a4baa799987866f3ed29c160535cd9b3075
tantarantantan <jbcarino@gmail.com>
2016-03-30 10:28:45 +0800

    jonathan's push

--- src/CloudPhoenix/CloudPhoenix.UI/Api/CompanyController.cs
+++ src/CloudPhoenix/CloudPhoenix.UI/Api/CompanyController.cs
@@ -15,12 +15,53 @@ namespace CloudPhoenix.UI.Api
         private CompanyRepository _comRepo;
         
 
-        public void GetAll()
+        public HttpResponseMessage GetAll()
         {
-            _comRepo = new CompanyRepository(new CloudPhoenixDbContext());
-            var comList = _comRepo.GetEntity("usp_company_list", new Dictionary<string, object>());
+            var uspParam = new Dictionary<string, object>();
+            var companies = "[]";
 
-            var companies =  JsonConvert.SerializeObject(comList.Tables[0]);
+            try
+            {
+                _comRepo = new CompanyRepository(new CloudPhoenixDbContext());
+                var comList = _comRepo.GetEntity("usp_company_list", new Dictionary<string, object>());
+
+                if (comList.Tables[0] != null)
+                {
+                    companies = JsonConvert.SerializeObject(comList.Tables[0]);
+                }
+
+                return this.Request.CreateResponse(HttpStatusCode.OK, new { company_list = companies });
+
+            }
+            catch (Exception ex)
+            {
+                return this.Request.CreateResponse(HttpStatusCode.BadRequest, ex.GetBaseException().Message);
+            }
         }
+
+        public HttpResponseMessage GetCompanyDetail(int id)
+        {
+            var uspParam = new Dictionary<string, object>();
+            var companyDetail = "[]";
+
+            try
+            {
+                _comRepo = new CompanyRepository(new CloudPhoenixDbContext());
+
+                uspParam.Add("@companyId", id);
+                var comDetail = _comRepo.GetEntity("usp_company_detail_get", uspParam);
+
+                if (comDetail.Tables[0] != null)
+                {
+                    companyDetail = JsonConvert.SerializeObject(comDetail.Tables[0]);
+                }
+                return this.Request.CreateResponse(HttpStatusCode.OK, new { company_detail = companyDetail });
+            }
+            catch (Exception ex)
+            {
+                return this.Request.CreateResponse(HttpStatusCode.BadRequest, ex.GetBaseException().Message);
+            }
+        }
+
     }
 }
