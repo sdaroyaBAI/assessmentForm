8248e16f3829893855921b90345e8546d1db2ca5
Shaun Daroya <sdaroya@blastasia.com>
2016-03-31 18:37:19 +0800

    added hidden 12th question

--- UI/partial/survey/survey.js
+++ UI/partial/survey/survey.js
@@ -20,28 +20,36 @@
                 });
 
             }.bind(this));
-            
-         self.shouldShow = function(i){
-            var myEl = document.getElementById('r1-2');
-             if(i>2 && i<11){
-                 if(myEl.checked){
-                    return true;
-                 }
-                 return false;
-             }     
-             return true;
-         }  
-       
         self.shouldShow = function(i){
             var myEl = document.getElementById('r1-2');
-            if(i>2 && i<11){
-                if(myEl.checked){
+            if(!myEl.checked){
+                if(i>2 && i<11){
+                    return false;
+                }
+                else if(i===11){
                     return true;
                 }
+            }
+            if(i===11){
                 return false;
-            }     
+            }
             return true;
-        }
+        };
+        /*self.shouldShow = function(i){
+            var myEl = document.getElementById('r1-2');
+            if(!myEl.checked){
+                if(i>2 && i<11){
+                    return false;
+                }
+                else if(i===11){
+                    return true;
+                }
+            }
+            if(i===11){
+                return false;
+            }
+            return true;
+        };*/
     }
     
     
