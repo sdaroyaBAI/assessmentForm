4de1d9ed2b7d013a22309f485dfdde41f0923492
Shaun Daroya <sdaroya@blastasia.com>
2016-04-01 12:15:25 +0800

    hidden radio buttons default on idk at survey

--- UI/partial/survey/survey.js
+++ UI/partial/survey/survey.js
@@ -3,10 +3,10 @@
     angular.module('CloudPhoenix').controller('SurveyCtrl', surveyController);
 
 
-    surveyController.$inject = ['surveyService', 'sharedService', '$scope','$stateParams'];
+    surveyController.$inject = ['surveyService', 'sharedService', '$scope','$stateParams', '$location'];
 
 
-    function surveyController(surveyService, sharedService, $scope,$stateParams) {
+    function surveyController(surveyService, sharedService, $scope,$stateParams,$location) {
         var self = this;
         self.questions = [];
         //self.companyID = sharedService.getCompanyID();
@@ -21,19 +21,31 @@
             }.bind(this));
 
         self.setAnswers = function(answers){
-            surveyService.setSurveyAnswers(JSON.stringify(answers));
+          surveyService.setSurveyAnswers(JSON.stringify(answers))
+            .then(function (response){
+            $location.path('/home');
+            });
         };
 
         self.shouldShow = function (i) {
-            var myEl = document.getElementById('r1-2');
-            if (!myEl.checked) {
+            var q3r1 = document.getElementById('r1-2');
+            var currElement1 = document.getElementById('r1-'+i);
+            var currElement2 = document.getElementById('r2-'+i);
+            var currElement3 = document.getElementById('r3-'+i);
+            if (!q3r1.checked) {
                 if (i > 2 && i < 11) {
+                    currElement1.checked = false;
+                    currElement2.checked = false;
+                    currElement3.checked = true;
                     return false;
                 } else if (i === 11) {
                     return true;
                 }
             }
             if (i === 11) {
+                currElement1.checked = false;
+                currElement2.checked = false;
+                currElement3.checked = true;
                 return false;
             }
             return true;
